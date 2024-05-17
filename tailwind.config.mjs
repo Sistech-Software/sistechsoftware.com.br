c4f26a1cbaf7b53585b27df7ccb52350e9e74607
Rodrigo Nunes <rodrigo@iuptec.com.br>
2024-05-17 19:45:49 -0300

    landing-sistech, feat: criacao da pagina da sistech

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary-color': '#0C0047',
+				'secundary-color': '#343434',
+				'tertiary-color': '#969696',
+				'quaternary-color': '#143CA1',
+				'neutral-color': '#F2F2FF',
+				'button1':'#143CA1',
+				'neutral':'#EAEAEA',
+				'footer':'#081840',
+				'footer-gradient':'#143CA1',
+				'font-btn':'#6A4BFF'
+		},
+		fontFamily: {
+			'be-vietnam': ['Be Vietnam', 'sans-serif'],
+		}
+	}
+},
+	variants: {},
+	plugins: [],
+}
