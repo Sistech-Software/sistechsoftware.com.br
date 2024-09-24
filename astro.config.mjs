c6068326a41227927c0f6a13402548165a48e0d6
Diego Dias Pereira <diego@iuptec.com.br>
2024-09-24 00:10:58 -0300

    fix: config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sistechsoftware.com.br',
+  base: 'sistechsoftware.com.br',
   integrations: [tailwind()]
 });
