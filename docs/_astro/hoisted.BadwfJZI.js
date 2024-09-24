5e53d3998bc7c8c22d1a1861b2a4e4011011e462
Diego Dias Pereira <diego@iuptec.com.br>
2024-09-23 23:48:22 -0300

    deploy: gh-pages

---
+++ docs/_astro/hoisted.BadwfJZI.js
@@ -0,0 +1,1 @@
+import"./Header.astro_astro_type_script_index_0_lang.DGxgQr4M.js";import"https://unpkg.com/imask";window.addEventListener("DOMContentLoaded",n=>{const e=document.getElementById("phone"),t={mask:"(00) 00000-0000"};IMask(e,t)});
